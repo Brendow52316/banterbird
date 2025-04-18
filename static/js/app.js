d2ccdec86e493a49a3a9a5b16b89d22199f03b4d
Brendow52316 <brendan.bezdecny@ascte.org>
2025-04-18 09:00:37 -0500

    done did stuff

--- static/js/app.js
+++ static/js/app.js
@@ -18,7 +18,7 @@ window.onload = async () => {
         const response = await fetch("/api/posts");
         const posts = await response.json();
         posts.forEach(post => renderPost(post));
-    } catch (error)
+    } catch (error){
     console.error("Error fetching posts:", error);
     }
 };
