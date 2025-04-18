f31309d0292aeb255f0807cfeba4f5236fb3f23b
Brendow52316 <brendan.bezdecny@ascte.org>
2025-04-18 08:49:10 -0500

    moving tweets to backend

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
-
+import json
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/api/posts')
+def get_posts():
+    with (open("data.json", "r")) as file:
+        posts = json.load(file)
+    return jsonify(posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
